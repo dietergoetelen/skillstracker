c14de7eaae7ebf58889c24aa4f63a5a3bf755c98
Dieter Goetelen <dieter.goetelen@hotmail.com>
2014-11-28 11:52:02 +0100

    common feature, main controller

---
+++ SkillsTracker.FrontEnd/src/app/common/controllers/mainController.js
@@ -0,0 +1,16 @@
+(function (app) {
+	'use strict';
+	
+	var MainController = (function () {
+		
+		function MainController() {
+			
+		}
+		
+		return MainController;
+		
+	}());
+	
+	app.controller('MainController', MainController);
+	
+}(angular.module('app.common')));

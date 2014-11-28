ce4ac3b30f1a71642c826cf3697076da074b0ae2
Dieter Goetelen <dieter.goetelen@hotmail.com>
2014-11-28 11:06:18 +0100

    init front end architecture

---
+++ SkillsTracker.FrontEnd/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	ngAnnotate = require('gulp-ng-annotate'),
+	express = require('express');
+
+
+var config = {
+	jsFiles: ['src/app/*.js','src/app/*/*.js', 'src/app/*/*/*.js'],
+	libs: ['src/js/angular.js', 'src/js/ui-router.js'],
+	less: ['less/*.less', 'less/*/*.less']
+};
+
+gulp.task('js', function () {
+	
+	gulp.src(config.jsFiles)
+		.pipe(concat('main.js'))
+		.pipe(ngAnnotate())
+		.pipe(gulp.dest('./src/js'));
+	
+});
+
+gulp.task('less', function () {
+	
+	gulp.src('less/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('./src/css'));
+	
+});
+
+gulp.task('libs', function () {
+	
+	gulp.src(config.libs)
+		.pipe(concat('libs.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./src/js'));
+	
+});
+
+gulp.task('develop', ['libs'], function () {
+	
+	gulp.watch(config.jsFiles, ['js']);
+	gulp.watch(config.less, ['less']);
+	
+});
---
+++ SkillsTracker.FrontEnd/src/js/main.js
@@ -0,0 +1,51 @@
+(function () {
+	'use strict';
+	
+	
+	var app = angular.module('app', [
+		'ui.router'
+	]);
+	
+	
+	app.run(
+		['$rootScope', function ($rootScope) {
+			$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+				
+				if (toState.data && toState.data.private && toState.data.private === true) {
+					
+				}
+				
+			});
+		}
+	]);
+}());
+(function () {
+	'use strict';
+	
+	var app = account.module('app.account', [
+		'ui.router'
+	]);
+	
+	app.config([
+		'$stateProvider', function ($stateProvider) {
+		
+			$stateProvider
+				.state('login', {
+					url: '/login',
+					templateUrl: 'app/account/views/login.html'
+				})
+				.state('register', {
+					url: '/register',
+					templateUrl: 'app/account/views/register.html'
+				})
+				.state('account', {
+					url: '/account',
+					data: {
+						private: true
+					},
+					templateUrl: 'app/account/views/account.html'
+				});
+		}
+	]);
+	
+}());

c78e7100412dc2edee253c7c75f0b1123c3c53b8
Bart-Beyers <bartbeyers@gmail.com>
2014-11-28 16:17:10 +0100

    Adding OAuth stuff

---
+++ SkillsTracker.API/App_Start/AutoFacConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using SkillsTracker.DAL;
+using SkillsTracker.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace SkillsTracker.API
+{
+    public class AutoFacConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<SkillsTrackerDbContext>().AsSelf().InstancePerRequest();
+            builder.RegisterGeneric(typeof(BaseRepository<>)).AsImplementedInterfaces().InstancePerRequest();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
---
+++ SkillsTracker.API/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillsTracker.API.Models
+{
+    public class UserModel
+    {
+        public string Password { get; set; }
+        public string Email { get; set; }
+        // rest
+    }
+}

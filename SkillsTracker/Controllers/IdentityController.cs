2cb11831cdc6b9ef8eef6adf0cf6f11ead6584bc
Bart-Beyers <bartbeyers@gmail.com>
2014-11-28 14:24:13 +0100

    remove skillstracker api

---
+++ SkillsTracker/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using SkillsTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace SkillsTracker.Controllers
+{
+    [Authorize]
+    public class IdentityController : ApiController
+    {
+        public IEnumerable<ViewClaim> Get()
+        {
+            var principal = Request.GetRequestContext().Principal as ClaimsPrincipal;
+
+            return principal.Claims.Select(c => new ViewClaim { Type = c.Type, Value = c.Value });
+        }
+    }
+}
---
+++ SkillsTracker/Provider/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SkillsTracker.Provider
+{
+    public class SimpleAuthorizationServerProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // secure storage: salt, iterate, yada
+            if (context.UserName != context.Password)
+            {
+                context.Rejected();
+                return;
+            }
+
+            var id = new ClaimsIdentity(context.Options.AuthenticationType);
+            id.AddClaim(new Claim("sub", context.UserName));
+            id.AddClaim(new Claim("role", "user"));
+
+            context.Validated(id);
+        }
+    }
+}

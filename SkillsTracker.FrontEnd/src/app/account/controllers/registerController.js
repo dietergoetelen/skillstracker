6153ddf07d1830d75f8f0e374ced7cce63a6f51e
Dieter Goetelen <dieter.goetelen@hotmail.com>
2014-11-28 12:07:40 +0100

    init register feature

---
+++ SkillsTracker.FrontEnd/src/app/account/controllers/registerController.js
@@ -0,0 +1,16 @@
+(function (app) {
+	'use strict';
+	
+	var RegisterController = (function () {
+		
+		function RegisterController() {
+			
+		}
+		
+		return RegisterController;
+		
+	}());
+	
+	app.controller('RegisterController', RegisterController);
+	
+}(angular.module('app.account')));
--- SkillsTracker.FrontEnd/src/js/main.js
+++ SkillsTracker.FrontEnd/src/js/main.js
@@ -105,6 +105,22 @@
 (function (app) {
 	'use strict';
 	
+	var RegisterController = (function () {
+		
+		function RegisterController() {
+			
+		}
+		
+		return RegisterController;
+		
+	}());
+	
+	app.controller('RegisterController', RegisterController);
+	
+}(angular.module('app.account')));
+(function (app) {
+	'use strict';
+	
 	var AccountService = (function () {
 		
 		function AccountService($http) {

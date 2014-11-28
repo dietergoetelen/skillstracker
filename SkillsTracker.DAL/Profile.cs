4f538288d6927566c44b2a9126ae6d3ee556672e
Dieter Goetelen <dieter.goetelen@hotmail.com>
2014-11-28 09:48:21 +0100

    resolved conflicts

---
+++ SkillsTracker.DAL/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkillsTracker.DAL
+{
+    public class Profile
+    {
+
+        public int Id { get; set; }
+
+        public string Introduction { get; set; }
+
+        public ICollection<string> PrimarySkills { get; set; }
+
+        [Column(TypeName="text")]
+        public string ProfileImage { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ SkillsTracker.DAL/User.cs
@@ -0,0 +1,19 @@
+﻿namespace SkillsTracker.DAL
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string EmployeeCode { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+
+
+    }
+}
